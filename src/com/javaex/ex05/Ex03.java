74b2736ff1491bdf7dc86e61089a34e2cc94ee05
wonho <wonho951@gmail.com>
2021-05-31 12:02:23 +0900

    2021.05.31 오전수업(배열)

---
+++ src/com/javaex/ex05/Ex03.java
@@ -0,0 +1,21 @@
+package com.javaex.ex05;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		int[] no = new int[6];  
+		
+		
+		for (int i = 0; i < 6; i++) {
+		no[i] = (int)(Math.random()*45)+1;  
+		}
+		
+		
+		for (int i = 0; i <= 6; i++) { // 1< 6       1<=6--->에러남. 방은 5개인데 6번째까지 찾으라해서 뇌절함.
+		System.out.println(no[i]);       
+		}
+
+
+	}
+
+}
