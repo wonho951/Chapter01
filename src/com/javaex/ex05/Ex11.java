58133d9b6e5c31ff6df299f2aa2b9046d97f6486
wonho <wonho951@gmail.com>
2021-06-01 13:09:45 +0900

    2021.06.01 java 프로그램이 실행시키는거

---
+++ src/com/javaex/ex05/Ex11.java
@@ -0,0 +1,22 @@
+package com.javaex.ex05;
+
+//java Ex11 -->원래 실행하던거.
+// java Ex11 i am choi won ho --> 이렇게 실행시킬거임
+
+//java 프로그램이 함--> String[] args = new String[5] 
+public class Ex11 {
+		
+	public static void main(String[] args) { // args는 java만든 사람들이 정해놓은거.변수명임.
+
+		//
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+
+		
+		
+		//System.out.println(args[1]);
+
+	}
+
+}
